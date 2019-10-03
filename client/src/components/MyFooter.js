91e0cc8db75240cd8cbc4504e2e38b37c662adfe
Aldo Solano <aldoglez34@gmail.com>
2019-10-03 00:11:29 -0500

    colors

--- client/src/components/MyFooter.js
+++ client/src/components/MyFooter.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const styles = {
   footer: {
-    backgroundColor: "dimgray",
+    backgroundColor: "#0c2c2c",
     marginTop: "auto"
   }
 };
@@ -14,10 +14,10 @@ function MyFooter() {
       className="mt-auto py-2 text-center text-light"
       style={styles.footer}
     >
-      <Container>
+      <Container className="text-light">
         <Row>
           <Col md={4} className="mt-3">
-            <h4>Ayuda</h4>
+            <h4 style={{ color: "#f3d084" }}>Ayuda</h4>
             <div>
               <p className="mb-0">
                 <a href="/" className="text-light">
@@ -32,7 +32,7 @@ function MyFooter() {
             </div>
           </Col>
           <Col md={4} className="mt-3">
-            <h4>Nosotros</h4>
+            <h4 style={{ color: "#f3d084" }}>Nosotros</h4>
             <div>
               <p className="mb-0">
                 <a href="/" className="text-light">
@@ -47,7 +47,7 @@ function MyFooter() {
             </div>
           </Col>
           <Col md={4} className="mt-3">
-            <h4>Envíos</h4>
+            <h4 style={{ color: "#f3d084" }}>Envíos</h4>
             <div>
               <p className="mb-0">
                 <a href="/" className="text-light">
@@ -66,17 +66,16 @@ function MyFooter() {
 
       <Container className="d-flex flex-row justify-content-center py-1 mt-4 text-light">
         <h5>
-          <small>© Complemento Natural, 2019</small>
+          <small style={{ color: "#c8c0b0" }}>© Complemento Natural</small>
         </h5>
-        <span className="mx-2">{"//"}</span>
-        <div>
-          <a href="/" className="text-light" title="Twitter">
+        <div className="ml-2">
+          <a style={{ color: "#c8c0b0" }} href="/" title="Twitter">
             <i className="fab fa-twitter mr-2" />
           </a>
-          <a href="/" className="text-light" title="Facebook">
+          <a style={{ color: "#c8c0b0" }} href="/" title="Facebook">
             <i className="fab fa-facebook-square mr-2" />
           </a>
-          <a href="/" className="text-light" title="Instagram">
+          <a style={{ color: "#c8c0b0" }} href="/" title="Instagram">
             <i className="fab fa-instagram" />
           </a>
         </div>
