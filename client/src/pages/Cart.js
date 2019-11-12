332399811b717d95b2ddfbc4243e5ad3c18ad57c
Aldo Solano González <aldoglez34@gmail.com>
2019-11-11 21:40:08 -0600

    cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-// import * as cartActions from "../redux-actions/cart";
+import { useSelector, useDispatch } from "react-redux";
+import * as cartActions from "../redux-actions/cart";
 import {
   Container,
   Row,
@@ -18,7 +18,7 @@ import MyBreadcrumb from "../components/MyBreadcrumb";
 function Cart() {
   const cartItems = useSelector(state => state.cart.items);
   const [products, setProducts] = useState([]);
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     // fetch all products in the shopping cart, one by one
@@ -33,13 +33,8 @@ function Cart() {
             let tempProduct = {};
             tempProduct._id = res.data._id;
             tempProduct.name = res.data.name;
-            tempProduct.content = res.data.content;
-            if (res.data.discount.hasDiscount) {
-              tempProduct.salePrice = res.data.discount.newPrice;
-            } else {
-              tempProduct.salePrice = res.data.salePrice;
-            }
             tempProduct.discount = res.data.discount;
+            tempProduct.salePrice = res.data.salePrice;
             // loop tempArr and find if the product exists, if so retrieve the index
             let indexOfProduct = tempArr.findIndex(
               p => p._id === tempProduct._id
@@ -48,11 +43,7 @@ function Cart() {
             if (indexOfProduct === -1) {
               // set the qty to 1 and the subtotal equals to the salePrice and then push the value
               tempProduct.qty = 1;
-              if (res.data.discount.hasDiscount) {
-                tempProduct.salePrice = res.data.discount.newPrice;
-              } else {
-                tempProduct.salePrice = res.data.salePrice;
-              }
+              tempProduct.subTotal = tempProduct.salePrice;
               tempArr.push(tempProduct);
             }
             // if indexOfProduct is different than -1, means the product was found
@@ -69,7 +60,6 @@ function Cart() {
     });
     // when its done fetching all products info,
     fetchAllProducts.then(tempArr => {
-      console.log(tempArr);
       setProducts(tempArr);
     });
   }, []);
@@ -86,59 +76,70 @@ function Cart() {
       <Container className="my-4">
         <h2 className="mb-1">Mi bolsa de compras</h2>
         <hr className="myDivider" />
-        <Row className="mt-4">
+        <Row className="mt-3">
+          {/* shopping bag col */}
           <Col md={8}>
-            <Table responsive size="sm">
+            <Table className="mt-1" responsive size="sm">
               <thead>
                 <tr>
-                  <th className="text-center border-top-0">Producto</th>
-                  {/* <th className="text-center border-top-0">Contenido</th> */}
-                  <th className="text-center border-top-0">Cantidad</th>
-                  <th className="text-center border-top-0">Precio</th>
-                  <th className="text-center border-top-0">Subtotal</th>
+                  <th className="text-center border-top-0 pb-2">Producto</th>
+                  <th className="text-center border-top-0 pb-2">Cantidad</th>
+                  <th className="text-center border-top-0 pb-2">Precio</th>
+                  <th className="text-center border-top-0 pb-2">Subtotal</th>
                 </tr>
               </thead>
               <tbody>
                 {cartItems.length === 0 ? (
                   <tr>
-                    <td className="bg-light text-center" colSpan="5">
+                    <td className="bg-light text-center pt-3" colSpan="5">
                       <em>Tu bolsa de compras está vacía</em>
                     </td>
                   </tr>
                 ) : products.length ? (
-                  products.map(p => {
-                    return (
-                      <tr key={p._id}>
-                        {/* name */}
-                        <td className="text-left">
-                          <a
-                            href={"/product/details/" + p._id}
-                            style={{ color: "#59a49a" }}
-                          >
-                            {p.name}
-                          </a>
-                          {p.discount.hasDiscount ? (
-                            <Badge pill className="ml-2" variant="warning">
-                              {p.discount.percentage}%
-                            </Badge>
-                          ) : null}
-                        </td>
-                        {/* content */}
-                        {/* <td className="text-left">{p.content}</td> */}
-                        {/* qty */}
-                        <td className="text-center">{p.qty}</td>
-                        {/* sale price */}
-                        <td className="text-center">
-                          {p.discount.hasDiscount ? (
-                            <>{p.discount.newPrice}</>
-                          ) : (
-                            <>{p.salePrice}</>
-                          )}
-                        </td>
-                        <td className="text-center">{p.subTotal}</td>
-                      </tr>
-                    );
-                  })
+                  <>
+                    {products.map(p => {
+                      return (
+                        <tr key={p._id}>
+                          {/* name */}
+                          <td className="text-left">
+                            <a
+                              href={"/product/details/" + p._id}
+                              className="text-dark"
+                            >
+                              {p.name}
+                            </a>
+                            {p.discount.hasDiscount ? (
+                              <Badge pill className="ml-2" variant="warning">
+                                {p.discount.percentage}%
+                              </Badge>
+                            ) : null}
+                          </td>
+                          {/* qty */}
+                          <td className="text-center">{p.qty}</td>
+                          {/* sale price */}
+                          <td className="text-center">
+                            {p.discount.hasDiscount ? (
+                              <>${p.discount.newPrice}</>
+                            ) : (
+                              <>${p.salePrice}</>
+                            )}
+                          </td>
+                          <td className="text-center">$ {p.subTotal}</td>
+                        </tr>
+                      );
+                    })}
+                    <tr>
+                      <td className="text-right" colSpan="5">
+                        <Button
+                          variant="link"
+                          className="text-danger"
+                          onClick={() => dispatch(cartActions.clear())}
+                        >
+                          Limpiar bolsa
+                        </Button>
+                      </td>
+                    </tr>
+                  </>
                 ) : (
                   <tr>
                     <td className="text-center" colSpan="5">
@@ -153,16 +154,74 @@ function Cart() {
               </tbody>
             </Table>
           </Col>
+          {/* card */}
           <Col md={4}>
             <Card>
-              <Card.Header as="h5">Resumen</Card.Header>
+              <Card.Header style={{ paddingBottom: "7px", paddingTop: "7px" }}>
+                <strong>Resumen</strong>
+              </Card.Header>
               <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                {cartItems.length ? (
+                  products.length ? (
+                    <>
+                      <Table>
+                        <tbody>
+                          <tr>
+                            <td className="border-top-0">
+                              Cantidad de productos
+                            </td>
+                            <td className="border-top-0">
+                              {products
+                                .map(p => p.qty)
+                                .reduce((prev, next) => prev + next)}
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Subtotal</td>
+                            <td>
+                              $
+                              {products
+                                .map(p => p.subTotal)
+                                .reduce((prev, next) => prev + next)}{" "}
+                              MXN
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Costo de envío</td>
+                            <td>$70 MXN</td>
+                          </tr>
+                          <tr>
+                            <td>Total general</td>
+                            <td>
+                              <strong>
+                                {" "}
+                                $
+                                {products
+                                  .map(p => p.subTotal)
+                                  .reduce((prev, next) => prev + next) +
+                                  70}{" "}
+                                MXN
+                              </strong>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </Table>
+                      <Button variant="danger">Pagar</Button>
+                    </>
+                  ) : (
+                    <div className="text-center">
+                      <Spinner
+                        className="spinnerStyle mt-3"
+                        animation="grow"
+                        role="status"
+                      />
+                    </div>
+                  )
+                ) : (
+                  <div className="text-center">
+                    <em>Tu bolsa de compras está vacía</em>
+                  </div>
+                )}
               </Card.Body>
             </Card>
           </Col>
