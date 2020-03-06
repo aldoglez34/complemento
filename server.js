25118345125f86bca231a39daca0a45ff2ab8588
Aldo Solano <aldoglez34@gmail.com>
2020-03-06 00:38:18 -0600

    msg

--- server.js
+++ server.js
@@ -34,7 +34,10 @@ app.get("*", (req, res) => {
 });
 
 // connect to the Mongo DB
-const uri = process.env.MONGODB_URI || "mongodb://localhost/complementoDB";
+// const uri = process.env.MONGODB_URI || "mongodb://localhost/complementoDB";
+const uri =
+  process.env.MONGODB_URI ||
+  "mongodb://aldo:ThirtyFour340815@ds213896.mlab.com:13896/heroku_zhf3ks7x";
 mongoose
   .connect(uri, {
     useNewUrlParser: true,
