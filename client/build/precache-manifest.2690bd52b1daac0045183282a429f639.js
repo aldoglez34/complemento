d0d8178a69c4f6d8de9f8edfd5c3c4b663178e85
Aldo Solano <aldoglez34@gmail.com>
2020-03-06 00:40:36 -0600

    msg

---
+++ client/build/precache-manifest.2690bd52b1daac0045183282a429f639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "101387080895b1c05a21",
+    "url": "/static/css/main.efaffbed.chunk.css"
+  },
+  {
+    "revision": "101387080895b1c05a21",
+    "url": "/static/js/main.60bcea1e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e12e61fe9207286c8ab0",
+    "url": "/static/js/2.aa4ab137.chunk.js"
+  },
+  {
+    "revision": "29388eab5b789a7e1f2c5efd12232ece",
+    "url": "/index.html"
+  }
+];
