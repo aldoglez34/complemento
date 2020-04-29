7268d5eee8a5e9ea74e03e95838a362478567f15
Aldo Solano González <aldoglez34@gmail.com>
2020-04-28 21:18:26 -0500

    msg

---
+++ client/build/precache-manifest.58fdb00032f76f2b571769d61850ee45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a555b8b2f1e9167d76ca",
+    "url": "/static/js/main.4464804e.chunk.js"
+  },
+  {
+    "revision": "c0a69ae17501e67d6bf3",
+    "url": "/static/js/2.8a98f0f9.chunk.js"
+  },
+  {
+    "revision": "a555b8b2f1e9167d76ca",
+    "url": "/static/css/main.80adf039.chunk.css"
+  },
+  {
+    "revision": "79df553c9e973852ceb1870e6b1cd382",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4c337b2747e2ab09d71e30e74f5f5e7.js"
+  "/precache-manifest.58fdb00032f76f2b571769d61850ee45.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
