6fbea95e29cb478e726bc139febb45a6181da125
Aldo Solano <aldoglez34@gmail.com>
2020-05-04 16:40:55 -0500

    msg

---
+++ client/build/precache-manifest.3f3fa83fc68b1db804c9c664e9831afc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c0868a9a151a993c60b",
+    "url": "/static/css/main.78e3ea7d.chunk.css"
+  },
+  {
+    "revision": "8c0868a9a151a993c60b",
+    "url": "/static/js/main.32b47acf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "33920c9a3f7aee80dcd6",
+    "url": "/static/js/2.ef9eee7e.chunk.js"
+  },
+  {
+    "revision": "75c771efa0c41479e46684785cfa1196",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b99f38b8b2a3a73a6c3379d5c2e73b8.js"
+  "/precache-manifest.3f3fa83fc68b1db804c9c664e9831afc.js"
 );
 
 workbox.clientsClaim();
