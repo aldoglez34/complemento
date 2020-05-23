69d37a370ccac427484e1daec4cea85dffc4fd67
Aldo Solano <aldoglez34@gmail.com>
2020-05-23 01:27:33 -0500

    msg

---
+++ client/src/manager/products/components/UploadImage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+const UploadImage = React.memo(() => {
+  return (
+    <Form.Group as={Col} md={6}>
+      <Form.Label>
+        <strong>Foto</strong>
+        <span title="Requerido" className="text-danger">
+          *
+        </span>
+      </Form.Label>
+      <Form.Control
+        disabled
+        type="text"
+        placeholder="Ingresa la foto"
+        name="photo"
+        value={values.photo}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        isValid={touched.photo && !errors.photo}
+        isInvalid={touched.photo && !!errors.photo}
+      />
+    </Form.Group>
+  );
+});
+
+export default UploadImage;
