6e3bbb497f80a23d49827eb6644c88057a765698
Aldo Solano González <aldoglez34@gmail.com>
2020-06-09 19:52:00 -0500

    msg

--- client/src/manager/sales/pages/SalesChart.js
+++ client/src/manager/sales/pages/SalesChart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ManagerLayout from "../../ManagerLayout";
 import Chart from "chart.js";
 import { Row, Col, Spinner } from "react-bootstrap";
@@ -6,7 +6,10 @@ import moment from "moment";
 import APIManager from "../../../utils/APIManager";
 
 const SalesChart = React.memo((props) => {
-  const [chartData, setChartData] = useState();
+  const [chartData, setChartData] = useState(null);
+
+  const chartContainer = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
 
   const type = props.routeProps.match.params.chart;
   const title =
@@ -20,19 +23,74 @@ const SalesChart = React.memo((props) => {
     // getting data first
     APIManager.mngr_currentWeekChart()
       .then((res) => {
-        setChartData(
-          res.data.reduce((acc, cv) => {
-            const key = cv.formattedDate;
+        const grouped = res.data.reduce((acc, cv) => {
+          const key = cv.formattedDate;
 
-            if (!acc[key]) {
-              acc[key] = [];
-            }
+          if (!acc[key]) {
+            acc[key] = [];
+          }
 
-            acc[key].push(cv);
+          acc[key].push(cv);
 
-            return acc;
-          }, [])
-        );
+          return acc;
+        }, []);
+        console.log("grouped", grouped);
+
+        const chartData = {
+          type: "line",
+          data: {
+            labels: [
+              "Lunes",
+              "Martes",
+              "Miércoles",
+              "Jueves",
+              "Viernes",
+              "Sábado",
+              "Domingo",
+            ],
+            datasets: [
+              {
+                data: [12, 19, 3, 5, 2, 3, 40],
+                backgroundColor: "rgba(255, 206, 86, 0.2)",
+                borderColor: "rgba(255, 206, 86, 1)",
+                label: "$",
+                pointBackgroundColor: "#f41206",
+                pointBorderColor: "#f75950",
+                pointRadius: 5,
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+              display: false,
+            },
+            tooltips: {
+              callbacks: {
+                label: function (tooltipItem) {
+                  return tooltipItem.yLabel;
+                },
+              },
+            },
+          },
+        };
+        console.log("chartData", chartData);
+
+        setChartData(chartData);
+        // setChartData(
+        //   res.data.reduce((acc, cv) => {
+        //     const key = cv.formattedDate;
+
+        //     if (!acc[key]) {
+        //       acc[key] = [];
+        //     }
+
+        //     acc[key].push(cv);
+
+        //     return acc;
+        //   }, [])
+        // );
       })
       .catch((err) => {
         console.log(err.data);
@@ -41,50 +99,17 @@ const SalesChart = React.memo((props) => {
           : alert("Ocurrió un error al cargar las ventas.");
       });
 
+    if (chartContainer && chartContainer.current && chartData) {
+      const newChartInstance = new Chart(chartContainer.current, chartData);
+      setChartInstance(newChartInstance);
+    }
+
     // building chart
-    const ctx = document.getElementById("myChart").getContext("2d");
+    // const ctx = document.getElementById("myChart").getContext("2d");
     // ctx.canvas.width = 800;
-    ctx.canvas.height = 420;
-    const myChart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: [
-          "Lunes",
-          "Martes",
-          "Miércoles",
-          "Jueves",
-          "Viernes",
-          "Sábado",
-          "Domingo",
-        ],
-        datasets: [
-          {
-            data: [12, 19, 3, 5, 2, 3, 40],
-            backgroundColor: "rgba(255, 206, 86, 0.2)",
-            borderColor: "rgba(255, 206, 86, 1)",
-            label: "$",
-            pointBackgroundColor: "#f41206",
-            pointBorderColor: "#f75950",
-            pointRadius: 5,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        legend: {
-          display: false,
-        },
-        tooltips: {
-          callbacks: {
-            label: function (tooltipItem) {
-              return tooltipItem.yLabel;
-            },
-          },
-        },
-      },
-    });
-  }, []);
+    // ctx.canvas.height = 420;
+    // const myChart = new Chart(ctx, chartConfig);
+  }, [chartContainer]);
 
   return (
     <ManagerLayout
@@ -103,7 +128,8 @@ const SalesChart = React.memo((props) => {
           <Row>
             {console.log(chartData)}
             <Col>
-              <canvas id="myChart"></canvas>
+              {/* <canvas id="myChart"></canvas> */}
+              <canvas ref={chartContainer} />
             </Col>
           </Row>
         </>
